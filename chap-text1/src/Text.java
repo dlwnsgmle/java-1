4074d61c0e86023438a14c9fa799f005589dba4b
USER <USER@AMP-19>
2019-05-16 10:50:59 +0900

    commit

---
+++ chap-text1/src/Text.java
@@ -0,0 +1,41 @@
+import javax.swing.JButton;
+
+public class Text  {
+	
+	 class 더하기{
+		private int a;
+		private int b;
+
+		public void  setValue(int a, int b) {
+			this.a =a;
+			this.b =b;
+		}
+		 class 빼기{
+			 private int a, num;
+				private int a11;
+				private int b;
+
+				public void  setValue(int a, int b) {
+					this.a =a;
+					this.num =num;
+					
+					class 나누기{
+						
+					}
+					class 곱하기 {
+						
+					}
+		
+	}
+				
+				
+		
+		
+		
+		
+		public void main(String[] args){
+       	  new Text ();
+       	  
+		}
+	
+
